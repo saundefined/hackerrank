18e2ea99d16826952c6df7e36ded1e4e54150332
Sergey Panteleev <sergey@s-panteleev.ru>
2019-03-26 09:04:24 +0300

    На PHP не было :(

---
+++ 30 Days of Code/Day 21: Generics.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct Printer<T> {
+	/**
+	*    Name: printArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+	
+	// Write your code here
+    func printArray<Element>(array: [Element]) {
+        for element in array {
+            print(element)
+        }
+    }
+    
+}
+
+
+var n = Int(readLine()!)!
+var intArray = Array(repeating: 0, count: n);
+for i in 0...n - 1 {
+	intArray[i] = Int(readLine()!)!;
+}
+
+n = Int(readLine()!)!
+var stringArray = Array(repeating: "", count: n);
+for i in 0...n - 1 {
+	stringArray[i] = readLine()!;
+}
+
+Printer<Int>().printArray(array: intArray)
+Printer<String>().printArray(array: stringArray)
