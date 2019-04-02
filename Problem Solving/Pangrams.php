4b8051a0b3ccb6a0c3b845593f659997ba58d9e6
Sergey Panteleev <sergey@s-panteleev.ru>
2019-04-02 22:05:58 +0300

    Pangrams

---
+++ Problem Solving/Pangrams.php
@@ -0,0 +1,31 @@
+<?php
+
+// Complete the pangrams function below.
+function pangrams($s)
+{
+    $letters = range('a', 'z');
+    $word = str_split(strtolower($s));
+
+    foreach ($word as $letter) {
+        $index = array_search($letter, $letters, false);
+        if ($index !== false) {
+            unset($letters[$index]);
+        }
+    }
+
+    return !empty($letters) ? 'not pangram' : 'pangram';
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+$s = '';
+fscanf($stdin, "%[^\n]", $s);
+
+$result = pangrams($s);
+
+fwrite($fptr, $result . "\n");
+
+fclose($stdin);
+fclose($fptr);
